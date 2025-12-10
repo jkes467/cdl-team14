2f4fc8635cfbf6b0eae876847308cf3d11d56abe
337mg096 <337mg096@ececomp3.ecn.purdue.edu>
2025-12-10 01:22:58 -0500

    waiting on controller and buffer

--- cdl/source/ai_accelerator.sv
+++ cdl/source/ai_accelerator.sv
@@ -14,6 +14,10 @@ module ai_accelerator #(
     output logic hready
 );
 
+logic [7:0] status_reg, error_reg, ctrl_reg;
+logic [63:0] bias_reg, output_data;
+logic handshake, is_weight, wr_en_push;
+
 ahb_sub subordinate(
         .clk(clk),
         .n_rst(n_rst),
@@ -29,7 +33,7 @@ ahb_sub subordinate(
         .hrdata(hrdata),
         .status_reg(status_reg),
         .output_data(output_data),
-        .err_reg(err_reg),
+        .err_reg(error_reg),
         .act_mode(act_mode),
         .handshake(handshake),
         .bias_reg(bias_reg),
@@ -38,5 +42,48 @@ ahb_sub subordinate(
         .is_weight(is_weight)
     );
 
+controller c0 (
+
+);
+
+sram_buffer buffer (
+
+);
+
+logic float;
+logic [7:0] load;
+logic [63:0] input_value;
+logic input_valid;
+logic output_valid;
+logic [63:0] output_value;
+
+systolic_array sys (
+    .clk(clk),
+    .n_rst(n_rst),
+    .float(float),
+    .load(load),
+    .input_value(input_value),
+    .input_valid(input_valid),
+    .output_valid(output_valid),
+    .output_value(output_value)
+);
+
+logic [63:0] bias_outputs;
+
+bias_adder bias (
+    .array_outputs(output_value), 
+    .bias(bias_reg),
+    .bias_outputs(bias_outputs), 
+    .float(float)
+);
+
+logic [63:0] activation_outputs
+
+activation act (
+    .biased_inputs(bias_outputs),
+    .activation_mode(act_mode),
+    .float(float),
+    .activated_outputs(activated_outputs)
+);
 endmodule
 
